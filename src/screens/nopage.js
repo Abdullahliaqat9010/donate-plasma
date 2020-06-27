09522c5489a88a2d8ab1d462a7d185be5feb7371
abdullah liaqat <abdullah.liaqat9010@gmail.com>
2020-06-27 14:22:50 +0500

    added 404 page

--- src/screens/nopage.js
+++ src/screens/nopage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import page404 from '../assets/404.svg'
 
 const NoPage = ()=> {
     return(
-        <p>NoPage</p>
+        <div style={{display:"flex",alignItems:"center",paddingTop:50,flexDirection:"column",justifyContent:"flex-start",textAlign:"center",height:600}} >
+            <Image width={300} height={300} src={page404} />
+            <h1 style={{color:"#f7464c"}} >404 Page not found</h1>
+        </div>
     )
 }
 
